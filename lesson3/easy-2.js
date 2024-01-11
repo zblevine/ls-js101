f49173ded2c9a5e2530d392fad3f5a3b4100d71f
Zach Levine <zblevine@gmail.com>
2024-01-11 03:17:32 -0500

    easy 2 done

--- lesson3/easy-2.js
+++ lesson3/easy-2.js
@@ -37,4 +37,35 @@ let arr1 = [1, 2, 3, 4, 5];
 arr1.splice(2, 1);
 console.log(arr1);
 
-//
+//q6
+
+const nested = ["Fred", "Wilma", ["Barney", "Betty"], ["Bambam", "Pebbles"]];
+console.log(nested.flat());
+console.log([].concat(...nested));
+
+//q7
+
+let flintstonesObj = { Fred: 0, Wilma: 1, Barney: 2,
+  Betty: 3, Bambam: 4, Pebbles: 5 };
+console.log(Object.entries(flintstonesObj).filter(pair => pair[0] === 'Barney').shift());
+
+//q8
+
+let nums = [1, 2, 3, 4]; // true
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+console.log(Array.isArray(nums));
+console.log(Array.isArray(table));
+
+//q9
+
+let title = "Flintstone Family Members";
+let lenToIndent = (40 - title.length) / 2;
+console.log(" ".repeat(lenToIndent) + title);
+
+//q10
+
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+console.log(statement1.split('').filter(char => char === 't').length);
+console.log(statement2.split('').filter(char => char === 't').length);
